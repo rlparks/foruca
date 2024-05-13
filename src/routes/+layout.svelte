6d7fdba8ccdb2044b01668e3363a45333e4bcf59
Rebecca Parks <rparkstx@gmail.com>
2024-05-13 14:31:20 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	interface Props {
+		data: LayoutData;
+		children: Snippet;
+	}
+
+	let { data, children }: Props = $props();
+</script>
+
+{@render children()}
