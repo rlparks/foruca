6f57900455a4d74f6da3f2dcfd0b650b99f44ebc
Rebecca Parks <rparkstx@gmail.com>
2024-05-13 21:05:39 -0400

    Add login input

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,3 +3,29 @@
 </script>
 
 <h2 class="text-center">Login</h2>
+
+<div class="center-h">
+	<form action="/login" method="post">
+		<label>Username<input type="text" name="username" /></label>
+		<label>Password<input type="password" name="password" /></label>
+		<button type="submit" class="button">Login</button>
+	</form>
+</div>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+	}
+
+	input {
+		margin: 0.5rem 0;
+		padding: 0.5rem;
+		border-radius: 5px;
+	}
+
+	button {
+		margin: 0.5rem 0;
+		padding: 0.5rem;
+	}
+</style>
