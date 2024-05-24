21dd2d9d760c102ecda6eb11f7fcf9d6121cb882
Rebecca Parks <rparkstx@gmail.com>
2024-05-24 11:05:46 -0400

    Initialize PB in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,84 @@
-import { PB_URL } from "$env/static/private";
+import { APP_URL, PB_URL, PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD } from "$env/static/private";
 import { getImageUrl } from "$lib/utils/helpers";
 import type { Handle } from "@sveltejs/kit";
 import PocketBase from "pocketbase";
+
+if (PB_ADMIN_EMAIL && PB_ADMIN_PASSWORD) {
+	try {
+		const tempPb = new PocketBase(PB_URL);
+		const admin = await tempPb.admins.create({
+			email: PB_ADMIN_EMAIL,
+			password: PB_ADMIN_PASSWORD,
+			passwordConfirm: PB_ADMIN_PASSWORD
+		});
+		initializeSchema(tempPb);
+	} catch (err) {
+		console.log("PB admin already exists");
+	}
+}
+
+async function initializeSchema(pb: PocketBase) {
+	await pb.admins.authWithPassword(PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD);
+
+	const settings = await pb.settings.update({
+		meta: {
+			appName: "foruca",
+			appUrl: APP_URL
+		}
+	});
+
+	try {
+		const users = await pb.collections.getOne("users");
+		const posts = await pb.collections.create({
+			name: "posts",
+			type: "base",
+			viewRule: "",
+			createRule: "@request.auth.id != ''",
+			schema: [
+				{
+					name: "title",
+					type: "text",
+					required: true
+				},
+				{
+					name: "body",
+					type: "text"
+				},
+				{
+					name: "owner",
+					type: "relation",
+					required: true,
+					options: {
+						collectionId: users.id,
+						maxSelect: 1
+					}
+				},
+				{
+					name: "time",
+					type: "date",
+					required: true
+				}
+			]
+		});
+
+		await pb.collections.update(posts.name, {
+			schema: [
+				...posts.schema,
+				{
+					name: "parent",
+					type: "relation",
+					required: false,
+					options: {
+						collectionId: posts.id,
+						maxSelect: 1
+					}
+				}
+			]
+		});
+	} catch (err) {
+		console.log("Error creating posts collection:", err);
+	}
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase(PB_URL);
