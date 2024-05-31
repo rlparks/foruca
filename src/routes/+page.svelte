205ff8e1912b7c1f9d335f76099ab68dd54efbd4
Rebecca Parks <rparkstx@gmail.com>
2024-05-31 14:14:52 -0400

    Add page navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
 	import { PostHolder } from "$lib/components";
 	import type { Post } from "$lib/types";
 
@@ -7,9 +8,13 @@
 	const posts: Post[] = $derived(data.posts);
 
 	const startingPostNumber = $derived((data.page - 1) * data.perPage + 1);
-	const endingPostNumber = $derived(
-		data.totalItems < data.perPage ? data.totalItems : data.page * data.perPage
-	);
+	const endingPostNumber = $derived(startingPostNumber + posts.length - 1);
+
+	$effect(() => {
+		if (posts.length === 0) {
+			goto(`/?page=${data.page - 1}`);
+		}
+	});
 </script>
 
 <svelte:head>
@@ -21,7 +26,38 @@
 		{#each posts as post (post.id)}
 			<PostHolder {post} />
 		{/each}
-
-		<p class="text-center">{`${startingPostNumber} - ${endingPostNumber} of ${data.totalItems}`}</p>
+		<div id="pagination">
+			{#if data.page > 1}
+				<a class="nav-link" href="/?page={data.page - 1}"> <div class="button">{"<"}</div></a>
+			{:else}
+				<div class="page-button"></div>
+			{/if}
+			<div class="center-v">
+				<p class="text-center">
+					{`${startingPostNumber} - ${endingPostNumber} of ${data.totalItems}`}
+				</p>
+			</div>
+			{#if endingPostNumber < data.totalItems}
+				<a class="nav-link" href="/?page={data.page + 1}"> <div class="button">{">"}</div></a>
+			{:else}
+				<div class="page-button"></div>
+			{/if}
+		</div>
 	</div>
 </div>
+
+<style>
+	p {
+		display: inline;
+		margin: 0;
+	}
+
+	#pagination {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.page-button {
+		width: 39px;
+	}
+</style>
