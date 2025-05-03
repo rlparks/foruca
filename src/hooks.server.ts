afbaef7274ce25b2fbe2cef1760c919bac452a3c
Rebecca Parks <git@rlparks.net>
2025-05-03 14:12:00 -0400

    Import hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,30 @@
+import type { Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
+
+const setLocals: Handle = async ({ event, resolve }) => {
+	const result = await resolve(event);
+
+	return result;
+};
+
+const setHeaders: Handle = async ({ event, resolve }) => {
+	const start = performance.now();
+
+	const result = await resolve(event);
+
+	if (!event.isSubRequest) {
+		console.log(
+			`${event.request.method} ${event.url.pathname}: ${(performance.now() - start).toFixed(2)}ms`,
+		);
+	}
+
+	result.headers.set("referrer-policy", "strict-origin-when-cross-origin");
+	result.headers.set("x-content-type-options", "nosniff");
+	result.headers.set("x-frame-options", "DENY");
+	result.headers.set("cross-origin-resource-policy", "same-origin");
+	result.headers.set("strict-transport-security", "max-age=63072000; includeSubDomains; preload");
+
+	return result;
+};
+
+export const handle = sequence(setLocals, setHeaders);
