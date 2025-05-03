17927a316786d8beb3503efad8e75d7b1a5eb836
Rebecca Parks <git@rlparks.net>
2025-05-03 10:24:19 -0400

    Add draft schema for OIDC account login

---
+++ src/lib/db/schema/0000_init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS account (
+    id TEXT PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    display_name TEXT NOT NULL,
+    is_admin BOOLEAN NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS session (
+    id TEXT PRIMARY KEY,
+    account_id TEXT NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    token_hash TEXT NOT NULL UNIQUE, -- user has the unhashed token as a cookie
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_activity_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_ip TEXT NOT NULL,
+    user_agent TEXT NOT NULL,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    oidc_id_token TEXT NOT NULL -- used to log the user out of the OIDC provider
+)
