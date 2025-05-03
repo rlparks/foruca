1b829cc242e094fd36249ccb0016824757e41e5a
Rebecca Parks <git@rlparks.net>
2025-05-03 14:05:29 -0400

    Only warn on unused

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import prettier from "eslint-config-prettier";
-import js from "@eslint/js";
 import { includeIgnoreFile } from "@eslint/compat";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
@@ -20,7 +20,11 @@ export default ts.config(
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node },
 		},
-		rules: { "no-undef": "off" },
+		rules: {
+			"no-undef": "off",
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": "warn",
+		},
 	},
 	{
 		files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
