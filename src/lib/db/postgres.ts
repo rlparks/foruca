d1b1baffe4bf9fbb843529eb94db61b8a80227da
Rebecca Parks <git@rlparks.net>
2025-05-03 13:39:23 -0400

    Add postgres client

---
+++ src/lib/db/postgres.ts
@@ -0,0 +1,7 @@
+import { env } from "$env/dynamic/private";
+import postgres from "postgres";
+
+const url = env.DATABASE_URL;
+if (!url) throw new Error("Environment variable DATABASE_URL is not set");
+
+export const sql = postgres(url, { transform: postgres.camel });
