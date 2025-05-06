a80fe07fdd920bbcafe031b9b546871cbffd5e64
Rebecca Parks <git@rlparks.net>
2025-05-06 07:04:32 -0400

    Maintain consistency with cookie - _

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export const OIDC_STATE_KEY = "foruca_oidc_state";
+export const OIDC_STATE_KEY = "foruca-oidc-state";
 
 /**
  * Formats a `Date` into something nice to look at.
