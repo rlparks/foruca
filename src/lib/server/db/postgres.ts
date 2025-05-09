52c3f819e1f2667d520646f499a1c053216610b8
Rebecca Parks <git@rlparks.net>
2025-05-08 22:01:58 -0400

    Don't error during build for missing env

--- src/lib/server/db/postgres.ts
+++ src/lib/server/db/postgres.ts
@@ -1,7 +1,9 @@
 import { env } from "$env/dynamic/private";
 import postgres from "postgres";
 
-const url = env.DATABASE_URL;
-if (!url) throw new Error("Environment variable DATABASE_URL is not set");
+const url = env.DATABASE_URL ?? "";
+if (!url) console.error("DATABASE_URL is not set!");
+// build spoinks...
+// if (!url) throw new Error("Environment variable DATABASE_URL is not set");
 
 export const sql = postgres(url, { transform: postgres.camel });
