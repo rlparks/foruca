9befdce8dba40016705960301c305dd7919b46c1
Rebecca Parks <git@rlparks.net>
2025-05-10 15:08:29 -0400

    Set pageTitle in layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,5 +3,5 @@ import type { LayoutServerLoad } from "./$types";
 export const load = (async (event) => {
 	const { account } = event.locals;
 
-	return { account };
+	return { account, pageTitle: "", pageDescription: "the forum application." };
 }) satisfies LayoutServerLoad;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,10 +2,7 @@ import { logoutUser } from "$lib/server/auth";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (event) => {
-	return {
-		pageTitle: "",
-		pageDescription: "the forum application.",
-	};
+	return {};
 };
 
 export const actions: Actions = {
