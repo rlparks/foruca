d9701db4107e4dd6fbe6f9e340593da444b048d2
Rebecca Parks <git@rlparks.net>
2025-05-11 13:49:39 -0400

    Make board list look fancier

--- src/routes/admin/boards/+page.svelte
+++ src/routes/admin/boards/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from "$lib/components/Button.svelte";
 	import HelpText from "$lib/components/HelpText.svelte";
 
 	let { data } = $props();
@@ -15,18 +16,36 @@
 			Create or update boards!
 		{/snippet}
 	</HelpText>
-	<div>
-		{#await data.boards}
-			<p>Loading boards...</p>
-		{:then boards}
-			{#each boards as board (board.id)}
-				<div>
-					<h2>{board.name}</h2>
-					<p>{board.description}</p>
-					<p>Created at: {board.createdAt}</p>
-					<p>Public: {board.isPublic}</p>
-				</div>
-			{/each}
+	<main class="w-full md:w-3/4 lg:w-4/5">
+		<div class="mb-4 flex items-center justify-between">
+			<h2 class="justify-baseline text-xl font-semibold text-gray-800">All Boards</h2>
+			<Button href="/admin/boards/create" color="green" font="base">Create Board</Button>
+		</div>
+
+		{#await data.boards then boards}
+			<div class="overflow-hidden rounded-lg bg-white shadow">
+				<ul class="divide-y divide-gray-200">
+					{#each boards as board (board.id)}
+						<li class="p-4 transition duration-150 ease-in-out hover:bg-gray-50">
+							<div class="mb-1 flex items-center justify-between">
+								<p class="text-lg font-semibold">
+									{board.name}
+								</p>
+								<span class="text-xs text-gray-500">{board.createdAt}</span>
+							</div>
+							<div class="text-sm text-gray-600">
+								<span>
+									{board.description}
+								</span>
+								<span class="mx-2 text-gray-300">|</span>
+								<span>1 mikbillion posts</span>
+							</div>
+						</li>
+					{:else}
+						<li class="p-4 text-center text-gray-500">No boards found. Try creating one?</li>
+					{/each}
+				</ul>
+			</div>
 		{/await}
-	</div>
+	</main>
 </div>
