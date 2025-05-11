ad7afb549a7c64fb4fa48ee6778f597858d425ef
Rebecca Parks <git@rlparks.net>
2025-05-11 12:26:23 -0400

    Add board description input

--- src/routes/admin/boards/create/+page.svelte
+++ src/routes/admin/boards/create/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
+	import Button from "$lib/components/Button.svelte";
 	import HelpText from "$lib/components/HelpText.svelte";
 	import Input from "$lib/components/Input.svelte";
 
@@ -17,16 +18,13 @@
 			Create a new board for very important posts.
 		{/snippet}
 	</HelpText>
-	<div>
-		<div>
-			<form method="POST" use:enhance>
-				<Input
-					label="Board Name"
-					name="boardName"
-					type="text"
-					helpText="Limited to 50 characters."
-				/>
-			</form>
-		</div>
+	<div class="flex w-full justify-center">
+		<form method="POST" use:enhance>
+			<Input label="Board Name" name="boardName" type="text" helpText="Limited to 50 characters." />
+
+			<Input label="Board Description" name="boardDescription" type="text" />
+
+			<Button type="submit" color="blue" font="small">Create Board</Button>
+		</form>
 	</div>
 </div>
