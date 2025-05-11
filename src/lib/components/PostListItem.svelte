4f2cd7305713f0541bee351cdf9d5a655f9c7d2a
Rebecca Parks <git@rlparks.net>
2025-05-11 18:54:40 -0400

    Make entire post preview clickable

--- src/lib/components/PostListItem.svelte
+++ src/lib/components/PostListItem.svelte
@@ -5,19 +5,20 @@
 	let { post }: { post: PostListPost } = $props();
 </script>
 
-<li class="p-4 transition duration-150 ease-in-out hover:bg-gray-50">
-	<div class="mb-1 flex items-center justify-between">
-		<a
-			href={`/post/${post.id}`}
-			class="text-lg font-semibold text-blue-600 hover:text-blue-800 hover:underline"
-		>
-			{post.title}
-		</a>
-		<span class="text-xs text-gray-500">{getFormattedDateTime(post.createdAt)}</span>
-	</div>
-	<div class="text-sm text-gray-600">
-		<span> by <span class="font-medium text-gray-700">{post.accountDisplayName}</span></span>
-		<span class="mx-1 text-gray-300">|</span>
-		<span>{post.replyCount} replies</span>
-	</div>
-</li>
+<div>
+	<a href={`/boards/${post.boardName}/${post.id}`}>
+		<li class="p-4 transition duration-150 ease-in-out hover:bg-gray-50">
+			<div class="mb-1 flex items-center justify-between">
+				<p class="text-lg font-semibold">
+					{post.title}
+				</p>
+				<span class="text-xs text-gray-500">{getFormattedDateTime(post.createdAt)}</span>
+			</div>
+			<div class="text-sm text-gray-600">
+				<span> by <span class="font-medium text-gray-700">{post.accountDisplayName}</span></span>
+				<span class="mx-1 text-gray-300">|</span>
+				<span>{post.replyCount} replies</span>
+			</div>
+		</li>
+	</a>
+</div>
