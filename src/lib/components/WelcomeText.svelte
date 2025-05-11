6627b2b4fa5c01ea398dd7ee69ccc0e387bda977
Rebecca Parks <git@rlparks.net>
2025-05-11 10:41:03 -0400

    Genericize HelpText

--- src/lib/components/WelcomeText.svelte
+++
@@ -1,6 +0,0 @@
-<aside class="w-full md:w-1/4 lg:w-1/5">
-	<h2 class="mb-4 border-b pb-2 text-lg font-semibold text-gray-700">
-		Welcome to <span class="text-blue-600">foruca</span>
-	</h2>
-	<p class="text-muted-foreground text-sm">The forum application that "works".</p>
-</aside>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import HelpText from "$lib/components/HelpText.svelte";
 	import PostList from "$lib/components/PostList.svelte";
-	import WelcomeText from "$lib/components/WelcomeText.svelte";
 
 	// let boards = $state([
 	// 	{ id: 1, name: "General Discussion" },
@@ -57,6 +57,13 @@
 <div
 	class="container mx-auto flex flex-col gap-6 px-4 py-6 sm:px-6 md:flex-row md:items-baseline lg:px-8"
 >
-	<WelcomeText />
+	<HelpText>
+		{#snippet title()}
+			Welcome to <span class="text-blue-600">foruca</span>
+		{/snippet}
+		{#snippet body()}
+			The forum application that "works".
+		{/snippet}
+	</HelpText>
 	<PostList {posts} boardName="All Posts" />
 </div>
