657f00b2077f4c200e6c592487ebe9900c00126d
Rebecca Parks <git@rlparks.net>
2025-05-11 13:12:46 -0400

    Get board creation request

--- src/routes/admin/boards/create/+page.server.ts
+++ src/routes/admin/boards/create/+page.server.ts
@@ -1,5 +1,17 @@
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load = (async () => {
 	return {};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async (event) => {
+		const formData = await event.request.formData();
+		const body = Object.fromEntries(formData.entries()) as {
+			boardName: string;
+			boardDescription: string;
+			privateBoard?: "on";
+		};
+		console.log(body);
+	},
+};
