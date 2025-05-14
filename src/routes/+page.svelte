ce3136a79858d812c26c1e2b73b821c40fa3c893
Rebecca Parks <git@rlparks.net>
2025-05-13 21:56:25 -0400

    Decrease vertical padding of main containers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-	class="container mx-auto flex flex-col gap-6 px-4 py-6 sm:px-6 md:flex-row md:items-baseline lg:px-8"
+	class="container mx-auto flex flex-col gap-6 px-4 py-5 sm:px-6 md:flex-row md:items-baseline lg:px-8"
 >
 	<HelpText>
 		{#snippet title()}
