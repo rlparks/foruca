09d620565e054c3103e453348a7e1ddc18d6476e
Rebecca Parks <git@rlparks.net>
2025-05-18 12:23:45 -0400

    Add detail to post header

--- src/routes/boards/[boardName]/[postId]/+page.svelte
+++ src/routes/boards/[boardName]/[postId]/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
+	import { getFormattedDateTime } from "$lib";
+
 	let { data } = $props();
 </script>
 
 <main class="w-full md:w-3/4 lg:w-4/5">
-	<div class="mb-4 flex min-h-10 items-center justify-between">
-		<h2 class="justify-baseline text-xl font-semibold text-gray-800">{data.post.title}</h2>
-		e
+	<div class="mb-4 min-h-10">
+		<div class="flex items-center justify-between">
+			<h2 class="justify-baseline text-xl font-semibold text-gray-800">{data.post.title}</h2>
+			<p class="text-gray-500">
+				{getFormattedDateTime(data.post.createdAt)}
+			</p>
+		</div>
+		<div class="flex items-center justify-between">
+			<p class="text-gray-500">
+				{data.post.accountDisplayName}
+			</p>
+			<p class="text-gray-500">1 mikbillion replies</p>
+		</div>
 	</div>
 
-	<div class="overflow-hidden rounded-lg bg-white shadow">
+	<div class="overflow-hidden rounded-lg bg-white p-4 shadow">
 		{data.post.body}
 	</div>
 </main>
