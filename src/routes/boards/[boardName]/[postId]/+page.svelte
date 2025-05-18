0c770a7381e7e0385bb32655b7bb88a818f6bf98
Rebecca Parks <git@rlparks.net>
2025-05-18 12:01:53 -0400

    Begin styling post page

--- src/routes/boards/[boardName]/[postId]/+page.svelte
+++ src/routes/boards/[boardName]/[postId]/+page.svelte
@@ -2,4 +2,13 @@
 	let { data } = $props();
 </script>
 
-<p>{JSON.stringify(data.post)}</p>
+<main class="w-full md:w-3/4 lg:w-4/5">
+	<div class="mb-4 flex min-h-10 items-center justify-between">
+		<h2 class="justify-baseline text-xl font-semibold text-gray-800">{data.post.title}</h2>
+		e
+	</div>
+
+	<div class="overflow-hidden rounded-lg bg-white shadow">
+		{data.post.body}
+	</div>
+</main>
