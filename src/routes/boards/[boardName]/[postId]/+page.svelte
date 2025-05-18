32c97ffb6cecbe8b273486faf68b11aacd8a24dd
Rebecca Parks <git@rlparks.net>
2025-05-18 13:17:25 -0400

    Rearrange post header on small devices

--- src/routes/boards/[boardName]/[postId]/+page.svelte
+++ src/routes/boards/[boardName]/[postId]/+page.svelte
@@ -10,17 +10,19 @@
 
 <main class="w-full md:w-3/4 lg:w-4/5">
 	<div class="mb-4 min-h-10">
-		<div class="flex items-center justify-between">
-			<h2 class="justify-baseline text-xl font-semibold text-gray-800">{data.post.title}</h2>
-			<p class="text-gray-500">
-				{getFormattedDateTime(data.post.createdAt)}
-			</p>
-		</div>
-		<div class="flex items-center justify-between">
-			<p class="text-gray-500">
-				{data.post.accountDisplayName}
-			</p>
-			<p class="text-gray-500">1 mikbillion replies</p>
+		<div class="flex flex-col items-center justify-between md:flex-row">
+			<div class="align-left w-full">
+				<h2 class="justify-baseline text-xl font-semibold text-gray-800">{data.post.title}</h2>
+				<p class="text-gray-500">
+					{data.post.accountDisplayName}
+				</p>
+			</div>
+			<div class="w-full md:text-right">
+				<p class="text-gray-500">
+					{getFormattedDateTime(data.post.createdAt)}
+				</p>
+				<p class="text-gray-500">1 mikbillion replies</p>
+			</div>
 		</div>
 	</div>
 
