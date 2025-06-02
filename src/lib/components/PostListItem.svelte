a0e13d1650b33ceb16379ccb8774427bfbb5397a
Rebecca Parks <git@rlparks.net>
2025-06-01 22:15:39 -0400

    Split post card with padding

--- src/lib/components/PostListItem.svelte
+++ src/lib/components/PostListItem.svelte
@@ -5,15 +5,19 @@
 	let { post, showBoardName }: { post: PostListPost; showBoardName: boolean } = $props();
 </script>
 
-<div>
+<li>
 	<a href={`/boards/${post.boardName}/${post.id}`}>
-		<li class="p-4 transition duration-150 ease-in-out hover:bg-gray-50">
+		<div class="p-4 pb-2 transition duration-150 ease-in-out hover:bg-gray-50">
 			<div class="mb-1 flex items-center justify-between">
 				<p class="truncate text-lg font-semibold">
 					{post.title}
 				</p>
 				<span class="text-xs text-gray-500">{getFormattedDateTime(post.createdAt)}</span>
 			</div>
+		</div>
+	</a>
+	<a href="heck">
+		<div class="p-4 pt-2 transition duration-150 ease-in-out hover:bg-gray-50">
 			<div class="text-sm text-gray-600">
 				<span>
 					by <span class="font-medium text-gray-700">{post.accountDisplayName}</span>
@@ -24,6 +28,6 @@
 				<span class="mx-1 text-gray-300">|</span>
 				<span>{post.replyCount} {post.replyCount === 1 ? "reply" : "replies"}</span>
 			</div>
-		</li>
+		</div>
 	</a>
-</div>
+</li>
