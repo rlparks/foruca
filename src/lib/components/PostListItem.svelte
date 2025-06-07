94bffe1b68dc1b7bf551116ab481d82840069cc1
Rebecca Parks <git@rlparks.net>
2025-06-06 20:33:45 -0400

    Add links for user and board on post item

--- src/lib/components/PostListItem.svelte
+++ src/lib/components/PostListItem.svelte
@@ -14,20 +14,25 @@
 				</p>
 				<span class="text-xs text-gray-500">{getFormattedDateTime(post.createdAt)}</span>
 			</div>
+			<span>{post.replyCount} {post.replyCount === 1 ? "reply" : "replies"}</span>
 		</div>
 	</a>
-	<a href="heck">
-		<div class="p-4 pt-2 transition duration-150 ease-in-out hover:bg-gray-50">
-			<div class="text-sm text-gray-600">
-				<span>
-					by <span class="font-medium text-gray-700">{post.accountDisplayName}</span>
-					{#if showBoardName}
-						in <span class="font-medium text-gray-700">{post.boardName}</span>
-					{/if}
-				</span>
-				<span class="mx-1 text-gray-300">|</span>
-				<span>{post.replyCount} {post.replyCount === 1 ? "reply" : "replies"}</span>
-			</div>
+	<div>
+		<div class="flex flex-col text-sm text-gray-600 md:flex-row">
+			<a
+				href="/accounts/{post.accountId}"
+				class="w-full p-4 pt-1 transition duration-150 ease-in-out hover:bg-gray-50 md:w-1/4"
+			>
+				by <span class="font-medium text-gray-700">{post.accountDisplayName}</span>
+			</a>
+			{#if showBoardName}
+				<a
+					href="/boards/{post.boardName}"
+					class="w-full p-4 pt-1 transition duration-150 ease-in-out hover:bg-gray-50 md:w-3/4"
+				>
+					in <span class="font-medium text-gray-700">{post.boardName}</span>
+				</a>
+			{/if}
 		</div>
-	</a>
+	</div>
 </li>
