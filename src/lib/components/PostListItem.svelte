a8f809c3b1b887250e12dfa29c12bfe4010957f8
Rebecca Parks <git@rlparks.net>
2025-06-06 20:06:22 -0400

    Revert "Split post card with height"
    This reverts commit b60284bcf2164734c4ae55e425873459652536fa.

--- src/lib/components/PostListItem.svelte
+++ src/lib/components/PostListItem.svelte
@@ -7,9 +7,7 @@
 
 <li>
 	<a href={`/boards/${post.boardName}/${post.id}`}>
-		<div
-			class="flex h-14 flex-col justify-center px-4 transition duration-150 ease-in-out hover:bg-gray-50"
-		>
+		<div class="p-4 pb-2 transition duration-150 ease-in-out hover:bg-gray-50">
 			<div class="mb-1 flex items-center justify-between">
 				<p class="truncate text-lg font-semibold">
 					{post.title}
@@ -19,10 +17,8 @@
 		</div>
 	</a>
 	<a href="heck">
-		<div
-			class="flex h-10 flex-col justify-center px-4 transition duration-150 ease-in-out hover:bg-gray-50"
-		>
-			<div class="flex align-middle text-sm text-gray-600">
+		<div class="p-4 pt-2 transition duration-150 ease-in-out hover:bg-gray-50">
+			<div class="text-sm text-gray-600">
 				<span>
 					by <span class="font-medium text-gray-700">{post.accountDisplayName}</span>
 					{#if showBoardName}
