cf8ce48af004752e0dfedd045f9b5880d741942f
Rebecca Parks <git@rlparks.net>
2025-06-08 11:44:10 -0400

    Alter schema to better-auth's liking

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -8,5 +8,52 @@ export const auth = betterAuth({
 			postgres: getInstance(true),
 		}),
 		type: "postgres",
+		casing: "snake", // doesn't seem to do anything?
+	},
+	user: {
+		fields: {
+			name: "name",
+			email: "email",
+			emailVerified: "email_verified",
+			image: "image",
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
+	},
+	session: {
+		fields: {
+			userId: "user_id",
+			token: "token",
+			expiresAt: "expires_at",
+			ipAddress: "ip_address",
+			userAgent: "user_agent",
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
+	},
+	account: {
+		fields: {
+			userId: "user_id",
+			accountId: "account_id",
+			providerId: "provider_id",
+			accessToken: "access_token",
+			refreshToken: "refresh_token",
+			idToken: "id_token",
+			accessTokenExpiresAt: "access_token_expires_at",
+			refreshTokenExpiresAt: "refresh_token_expires_at",
+			scope: "scope",
+			password: "password",
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
+	},
+	verification: {
+		fields: {
+			identifier: "identifier",
+			value: "value",
+			expiresAt: "expires_at",
+			createdAt: "created_at",
+			updatedAt: "updated_at",
+		},
 	},
 });
