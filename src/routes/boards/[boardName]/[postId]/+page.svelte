1c1599d5d4911119e8e06e3fc45083295795dc1c
Rebecca Parks <git@rlparks.net>
2025-08-03 19:15:37 -0400

    Hide reply button when no user

--- src/routes/boards/[boardName]/[postId]/+page.svelte
+++ src/routes/boards/[boardName]/[postId]/+page.svelte
@@ -30,44 +30,46 @@
 <div class="mb-6 rounded-lg outline">
 	<p class="p-4">{data.post.body}</p>
 
-	{#if !isReplying}
-		<button
-			onclick={() => (isReplying = true)}
-			class="w-full cursor-pointer bg-gray-50 p-4 text-center transition hover:bg-gray-100 dark:bg-gray-950 dark:hover:bg-gray-900"
-		>
-			Reply
-		</button>
-	{:else}
-		<form
-			bind:this={formEl}
-			class="bg-gray-50 p-4 pt-3 dark:bg-gray-950"
-			method="POST"
-			transition:slide
-			use:enhance={() => {
-				return async (form) => {
-					if (form.result.type !== "failure") {
-						isReplying = false;
-					}
-					await form.update();
-				};
-			}}
-		>
-			<TextArea
-				name="body"
-				rows={5}
-				label="Reply"
-				helpText={form?.message}
-				onkeydown={(e) => {
-					if (e.ctrlKey && e.key === "Enter") {
-						formEl?.requestSubmit();
-					}
+	{#if data.user}
+		{#if !isReplying}
+			<button
+				onclick={() => (isReplying = true)}
+				class="w-full cursor-pointer bg-gray-50 p-4 text-center transition hover:bg-gray-100 dark:bg-gray-950 dark:hover:bg-gray-900"
+			>
+				Reply
+			</button>
+		{:else}
+			<form
+				bind:this={formEl}
+				class="bg-gray-50 p-4 pt-3 dark:bg-gray-950"
+				method="POST"
+				transition:slide
+				use:enhance={() => {
+					return async (form) => {
+						if (form.result.type !== "failure") {
+							isReplying = false;
+						}
+						await form.update();
+					};
 				}}
-			/>
-			<Button class="mb-2" color="blue" font="small" type="submit">Submit</Button>
-			<Button color="outline" font="small" type="button" onclick={() => (isReplying = false)}>
-				Cancel
-			</Button>
-		</form>
+			>
+				<TextArea
+					name="body"
+					rows={5}
+					label="Reply"
+					helpText={form?.message}
+					onkeydown={(e) => {
+						if (e.ctrlKey && e.key === "Enter") {
+							formEl?.requestSubmit();
+						}
+					}}
+				/>
+				<Button class="mb-2" color="blue" font="small" type="submit">Submit</Button>
+				<Button color="outline" font="small" type="button" onclick={() => (isReplying = false)}>
+					Cancel
+				</Button>
+			</form>
+		{/if}
 	{/if}
 </div>
 
