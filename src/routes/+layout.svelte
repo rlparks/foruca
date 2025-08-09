5f2a99bc28c54f568a452f5c2e23ad1665a90823
Rebecca Parks <git@rlparks.net>
2025-08-09 11:02:24 -0400

    Add Rybbit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import { dev } from "$app/environment";
+	import { env } from "$env/dynamic/public";
 	import "../app.css";
 	import Sidebar from "./Sidebar.svelte";
 
 	let { children, data } = $props();
 </script>
+
+<svelte:head>
+	<!-- rybbit -->
+	<script
+		src="https://frog.rebeccaparks.com/api/script.js"
+		data-site-id={env.PUBLIC_RYBBIT_ID}
+		data-session-replay="true"
+		data-web-vitals="true"
+		data-track-errors="true"
+		defer
+	></script>
+</svelte:head>
 
 <div class="flex min-h-dvh">
 	<div class="sticky top-0 hidden h-dvh w-[200px] flex-shrink-0 md:block">
